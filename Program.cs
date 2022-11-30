44479d6b77d5be1b0c2edef980ab021ead1842d4
GustavJohansson420 <gujo04002@utb.vaxjo.se>
2022-11-30 21:44:02 +0100

    yeye

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿class Program 
+{
+    static void Main()
+    {
+        int resultat;
+        if (!int.TryParse(Console.ReadLine(), out resultat))
+            Console.WriteLine("Skriv en siffra");
+    }
+    public static bool TryParse(string s, out int result)
+    {
+        
+    }   
+}
